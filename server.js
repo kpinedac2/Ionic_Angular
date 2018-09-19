5afee8f570c4ed41d616693e13f4b922029e47cb
kpinedac2 <42192434+kpinedac2@users.noreply.github.com>
2018-09-19 00:45:15 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require ('express'); 
-var path = require ('ruta') 
+var path = require ('https://ionicpuntoventa.herokuapp.com') 
 var app = express (); 
 
 app.use (express.static (path.resolve (__dirname, "www"))); 
 app.set ('port', process.env.PORT || 3000); 
 app.listen (app.get ('port'), function () { 
-console.log ("escuchando Puerto", app.get ("puerto")); 
+console.log ("escuchando Puerto", app.get ("port")); 
 }); 
